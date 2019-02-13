e4a9760515f1d33f3115ada239542dcf142c8368
Mark Pothecary <markpothers@hotmail.com>
2019-02-13 17:06:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
-      holiday.each do |holiday, supply|
+      season.each do |holiday, supply|
         puts "#{holiday.to_s.capitalize}: #{supply.capitalize}"
       end
   end

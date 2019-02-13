dd1eca21bc2ea742c3a8b5c1db9378fb003c7ff4
Mark Pothecary <markpothers@hotmail.com>
2019-02-13 19:51:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,5 +82,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+    BBQ_hols = []
+    holiday_hash.each do |season, holiday|
+      holiday.each do |holiday, supply|
+          if (supply.any? do { |item| item == "BBQ" }) == true
+      BBQ_hols << holiday
+    end
+  BBQ_hols
 end
